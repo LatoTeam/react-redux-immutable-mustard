5e55988897591a5b0d5776dc157abbeaa5641d08
Riccardo-Zanutta <zanuttar@gmail.com>
2016-08-01 20:29:35 +0200

    Fix to eslint config, Rename components, fix lint typos, creating mock APIs

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import Main from '../components/Main';
+import App from '../components/App';
 import NotFound from '../components/NotFound';
 import HelloWorld from '../components/HelloWorld';
 
 const routes = (
-  <Route path="/" component={Main}>
+  <Route path="/" component={App}>
     <IndexRoute component={HelloWorld} />
     <Route path="*" component={NotFound} status={404} />
   </Route>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import routes from './config/routes';
 
+// if you want to active the Redux dev tools, make sure to include
+// window.devToolsExtension && window.devToolsExtension() as a parameter of the
+// configureStore function.
 const store = configureStore();
 
 render(
